0d7d5c4116ce10d7f08670c547d8eeac30e87a36
LeeVanie <942629585@qq.com>
2016-12-06 20:02:53 +0800

    aaaaaaa

--- app/src/main/java/aiyiqi/bwf/com/yiqizhuangxiu/adapter/adapter/NewRecycleviewAdapter.java
+++ app/src/main/java/aiyiqi/bwf/com/yiqizhuangxiu/adapter/adapter/NewRecycleviewAdapter.java
@@ -2,15 +2,12 @@ package aiyiqi.bwf.com.yiqizhuangxiu.adapter.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import aiyiqi.bwf.com.yiqizhuangxiu.R;
 import aiyiqi.bwf.com.yiqizhuangxiu.entity.Response_New;
@@ -21,37 +18,62 @@ import butterknife.ButterKnife;
  * Created by Administrator on 2016/11/25.
  */
 
-public class NewRecycleviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private LayoutInflater inflater;
-    private List<Response_New.DataBean> dataBeens;
+public class NewRecycleviewAdapter extends BaseMainRecycleViewAdapter<Response_New.DataBean> {
+
+    private static final int TYPE_CONTENT = 0;
+    private static final int TYPE_FOOTER = 1;
+
+    private Context context;
+
     public NewRecycleviewAdapter(Context context) {
-        dataBeens = new ArrayList<>();
-        inflater = LayoutInflater.from(context);
+        super(context);
+        this.context = context;
     }
-    public void addDatas(List<Response_New.DataBean> dataBeens){
-        this.dataBeens.addAll(dataBeens);
-        notifyDataSetChanged();
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1)
+            return TYPE_FOOTER;
+        return TYPE_CONTENT;
     }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.main_new, parent, false);
-        return new NewViewHolder(view);
+        if (viewType == TYPE_CONTENT)
+            return new NewViewHolder(inflaterView(R.layout.fragment2_new, parent));
+        return new FooterHolder(inflaterView(R.layout.subview_footer_loader, parent));
     }
+
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        if (position < getHeaderItemCount())
+            return;
+        if (position >= getItemCount() - getFooterItemCount()) {
+            bindFooterViewHolder(holder, position);
+            return;
+        }
+
         NewViewHolder newViewHolder = (NewViewHolder) holder;
-        Response_New.DataBean dataBean = dataBeens.get(position);
+        Response_New.DataBean dataBean = getItem(position);
         newViewHolder.imageViewAuthor.setImageURI(dataBean.getAvtUrl());
         newViewHolder.authorName.setText(dataBean.getSubject());
         newViewHolder.subjectName.setText(dataBean.getAuthor());
-        newViewHolder.lookedtext.setText(dataBean.getViews()+"");
+        newViewHolder.lookedtext.setText(dataBean.getViews() + "");
         newViewHolder.times.setText(dataBean.getDateline());
     }
+
     @Override
-    public int getItemCount() {
-        return dataBeens.size();
+    protected int getHeaderItemCount() {
+        return 0;
     }
-    public static class NewViewHolder extends RecyclerView.ViewHolder{
+
+    @Override
+    protected int getFooterItemCount() {
+        return 1;
+    }
+
+    public static class NewViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.author_name)
         TextView authorName;
         @BindView(R.id.subject_name)
@@ -62,6 +84,9 @@ public class NewRecycleviewAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         SimpleDraweeView imageViewAuthor;
         @BindView(R.id.times)
         TextView times;
+        @BindView(R.id.image_framgent)
+        ImageView imageFramgent;
+
         public NewViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
